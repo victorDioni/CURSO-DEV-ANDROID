718a83ff9851701ebdbc1312a145525cd50c8b32
Victor Dionizio <victor.dionizio28@gmail.com>
2021-06-12 23:45:00 -0300

    Escolhendo opcao App3 Jokenpo

--- Secao 10  - Java POO/Jokenpo/app/src/main/java/dionizio/victor/jokenpo/MainActivity.java
+++ Secao 10  - Java POO/Jokenpo/app/src/main/java/dionizio/victor/jokenpo/MainActivity.java
@@ -3,6 +3,7 @@ package dionizio.victor.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+
     }
 }
