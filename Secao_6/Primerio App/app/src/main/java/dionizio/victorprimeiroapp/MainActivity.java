274e84e000084720dce2688aeac32a52c5763870
Victor Dionizio <victor.dionizio28@gmail.com>
2021-08-16 21:11:26 -0300

    Meu primeiro App

---
+++ Secao_6/Primerio App/app/src/main/java/dionizio/victorprimeiroapp/MainActivity.java
@@ -0,0 +1,21 @@
+package dionizio.victorprimeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+        TextView texto = findViewById(R.id.txtTexto);
+        texto.setText("Victor Dionizio");
+    }
+}
