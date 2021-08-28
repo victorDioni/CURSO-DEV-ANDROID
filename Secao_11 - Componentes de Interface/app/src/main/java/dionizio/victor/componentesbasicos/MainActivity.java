627081f8c984ec913aacab4650eed065745f1695
Victor Dionizio <victor.dionizio28@gmail.com>
2021-08-27 21:50:28 -0300

    Recuperando valores de uma caixa de texto

--- Secao_11 - Componentes de Interface/app/src/main/java/dionizio/victor/componentesbasicos/MainActivity.java
+++ Secao_11 - Componentes de Interface/app/src/main/java/dionizio/victor/componentesbasicos/MainActivity.java
@@ -3,12 +3,37 @@ package dionizio.victor.componentesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText campoNome;
+    private TextView campoResultado;
+    private TextInputEditText campoEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campoNome = findViewById(R.id.txtNome);
+        campoResultado = findViewById(R.id.txtResultado);
+        campoEmail = findViewById(R.id.txtEmail);
+    }
+
+    public void enviar(View view){
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        campoResultado.setText("nome: " + nome + " " + "email: " +email);
+    }
+
+    public void limpar(View view){
+        campoNome.setText("");
+        campoEmail.setText("");
+        campoResultado.setText("");
     }
 }
