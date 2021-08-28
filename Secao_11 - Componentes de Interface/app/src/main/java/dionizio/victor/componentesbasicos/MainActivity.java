ad403ba8a1f3ad577ac531d19791e6c01cb2a0f7
Victor Dionizio <victor.dionizio28@gmail.com>
2021-08-27 22:50:08 -0300

    Botao de opcao

--- Secao_11 - Componentes de Interface/app/src/main/java/dionizio/victor/componentesbasicos/MainActivity.java
+++ Secao_11 - Componentes de Interface/app/src/main/java/dionizio/victor/componentesbasicos/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -19,19 +21,49 @@ public class MainActivity extends AppCompatActivity {
     // CAIXA DE SELECAO
     private CheckBox checkVerde, checkBranco, checkVermelho;
 
+    // BOTÃO DE OPÇÃO
+    private RadioButton rdbMasculino, rdbFeminino;
+    private RadioGroup opcSexo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        campoNome = findViewById(R.id.txtNome);
-        campoResultado = findViewById(R.id.txtResultado);
-        campoEmail = findViewById(R.id.txtEmail);
+        campoNome       = findViewById(R.id.txtNome);
+        campoResultado  = findViewById(R.id.txtResultado);
+        campoEmail      = findViewById(R.id.txtEmail);
 
-        checkVerde = findViewById(R.id.checkVerde);
-        checkBranco = findViewById(R.id.checkBranco);
-        checkVermelho = findViewById(R.id.checkVermelho);
+        checkVerde      = findViewById(R.id.checkVerde);
+        checkBranco     = findViewById(R.id.checkBranco);
+        checkVermelho   = findViewById(R.id.checkVermelho);
+
+        rdbMasculino    = findViewById(R.id.radioButtonMasculino);
+        rdbFeminino     = findViewById(R.id.radioButtonFeminino);
+        opcSexo         = findViewById(R.id.opcSexo);
+        raddioButton();
+    }
+
+    public void raddioButton(){
+        opcSexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == R.id.radioButtonMasculino){
+                    campoResultado.setText("Masculino selecionado");
+                }else if(i == R.id.radioButtonFeminino){
+                    campoResultado.setText("Feminino selecionado");
+                }
+            }
+        });
+        /*
+        if(rdbMasculino.isChecked()){
+            campoResultado.setText("Masculino selecionado");
+        }else{
+            campoResultado.setText("Feminino selecionado");
+        }
+         */
+
     }
 
     public void checkbox(){
@@ -48,7 +80,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void enviar(View view){
-        checkbox();
+        raddioButton();
+//        checkbox();
         /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
