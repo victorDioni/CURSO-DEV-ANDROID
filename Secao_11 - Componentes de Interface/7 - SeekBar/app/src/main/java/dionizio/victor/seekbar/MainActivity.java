4df36b1d37424fa26495e5511ce7d7a7dcbcb3e7
Victor Dionizio <victor.dionizio28@gmail.com>
2021-09-17 09:07:23 -0300

    SeekBar

---
+++ Secao_11 - Componentes de Interface/7 - SeekBar/app/src/main/java/dionizio/victor/seekbar/MainActivity.java
@@ -0,0 +1,43 @@
+package dionizio.victor.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar         = findViewById(R.id.seekBarEscala);
+        txtResultado    = findViewById(R.id.txtResultado);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtResultado.setText("Progresso: " + i + " / " + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void recuperarProgresso(View view){
+        txtResultado.setText("Escolhido: " + seekBar.getProgress());
+    }
+}
