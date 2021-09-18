cd503e3197515b65d8aab4b39d0c6019802204bc
Victor Dionizio <victor.dionizio28@gmail.com>
2021-09-18 15:59:25 -0300

    RecyclerView Parte 1

---
+++ Secao_12 - Componentes de listagem/2 - Recycler View/app/src/main/java/dionizio/victor/recyclerview/MainActivity.java
@@ -0,0 +1,27 @@
+package dionizio.victor.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView    = findViewById(R.id.recyclerView);
+
+        //CONFIGURAR ADAPTER
+        
+
+        //CONFIGURAR RECYCLERVIEW
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+    }
+}
