d0f5968e9ce415f4450f7bdb1ab4199adf2b2f58
Victor Dionizio <victor.dionizio28@gmail.com>
2021-10-12 18:55:30 -0300

    Estrutura do NavigationDrawer

---
+++ Secao_13_Navegacao_Activities_Fragments/6 - Navigation Drawer/app/src/main/java/dionizio/victor/navigationdrawer/MainActivity.java
@@ -0,0 +1,60 @@
+package dionizio.victor.navigationdrawer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import dionizio.victor.navigationdrawer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        //CRIA REFERENCIA PARA TODA A AREA DO NAVIGATIONDRAWER
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        //CRICA REFERENCIA PARA A AREA DE NAVEGAÇÃO
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        //DEFINE CONFIGURAÇÕES DO NAVIGATIONDRAWER
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+
+        // CONFIGURA A AREA QUE IRA CARREGAR OS FRAGMENTS
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        // EXIBE O MENU SUPERIOR DE NAVEFAÇÃO - MENU HAMBURGUER
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+        // CONFIGURA A NAVEGAÇÃO PARA O NAVIGATIONVIEW - CARREGA OS ITENS DE MENU QUE SÃO SELECIONADOS
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
