42feb8191f99aeb7b142f7363f8eebccc353112a
Victor Dionizio <victor.dionizio28@gmail.com>
2021-10-16 13:25:47 -0300

    Criando pagina sobre no App ATM Consultoria

--- Secao_13_Navegacao_Activities_Fragments/7 - ATM/app/src/main/java/dionizio/victor/atmconsultoria/ui/sobre/SobreFragment.java
+++ Secao_13_Navegacao_Activities_Fragments/7 - ATM/app/src/main/java/dionizio/victor/atmconsultoria/ui/sobre/SobreFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import dionizio.victor.atmconsultoria.R;
+import mehdi.sakout.aboutpage.AboutPage;
 
 public class SobreFragment extends Fragment {
 
@@ -19,7 +20,21 @@ public class SobreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sobre, container, false);
+        return new AboutPage(getActivity())
+                .setImage(R.drawable.logo)
+
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@atmconsultoria.com.br", "Envie um e-mail")
+                .addWebsite("https://www.google.com/", "Acesse nosso site")
+
+                .addGroup("Redes Sociais")
+                .addFacebook("victor.dionizio.3", "Facebook")
+                .addInstagram("dionizio.v","Instagram")
+                .addTwitter("https://twitter.com","Twiter")
+                .addYoutube("https://www.youtube.com/","Youtube")
+                .addGitHub("victorDioni","GitHub")
+                .addPlayStore("tv.twitch.android.app","Download App")
+
+                .create();
     }
 }
