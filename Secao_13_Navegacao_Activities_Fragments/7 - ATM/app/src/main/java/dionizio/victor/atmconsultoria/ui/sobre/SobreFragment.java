9ff0fd1d2e194a85242ab8c52b2f9130b8b966ac
Victor Dionizio <victor.dionizio28@gmail.com>
2021-10-16 11:22:57 -0300

    Adicionando conteudos no App ATM Consultoria e alterando Themas

---
+++ Secao_13_Navegacao_Activities_Fragments/7 - ATM/app/src/main/java/dionizio/victor/atmconsultoria/ui/sobre/SobreFragment.java
@@ -0,0 +1,25 @@
+package dionizio.victor.atmconsultoria.ui.sobre;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dionizio.victor.atmconsultoria.R;
+
+public class SobreFragment extends Fragment {
+
+    public SobreFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sobre, container, false);
+    }
+}
