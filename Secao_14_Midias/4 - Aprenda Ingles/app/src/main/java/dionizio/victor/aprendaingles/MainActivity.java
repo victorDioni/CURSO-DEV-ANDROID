16b5035966c5949345ba93be88589589e05bf581
Victor Dionizio <victor.dionizio28@gmail.com>
2021-10-27 19:39:29 -0300

    Criando interface de Abas - App Aprenda Ingles

---
+++ Secao_14_Midias/4 - Aprenda Ingles/app/src/main/java/dionizio/victor/aprendaingles/MainActivity.java
@@ -0,0 +1,45 @@
+package dionizio.victor.aprendaingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import dionizio.victor.aprendaingles.Fragment.BichosFragment;
+import dionizio.victor.aprendaingles.Fragment.NumerosFragment;
+import dionizio.victor.aprendaingles.Fragment.VogaisFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout  = findViewById(R.id.viewPagerTab);
+        viewPager       = findViewById(R.id.viewPager);
+
+        // Remove sobra da ActionBar
+        getSupportActionBar().setElevation(0);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Bichos", BichosFragment.class)
+                .add("Números", NumerosFragment.class)
+                .add("Vogais", VogaisFragment.class)
+                .create());
+
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
+
+
+    }
+}
