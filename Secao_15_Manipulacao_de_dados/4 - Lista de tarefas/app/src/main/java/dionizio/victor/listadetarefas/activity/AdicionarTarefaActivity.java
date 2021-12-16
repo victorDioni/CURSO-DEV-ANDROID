c3505f98f210e2780259d055b2b308733153897e
Victor Dionizio <victor.dionizio28@gmail.com>
2021-12-16 12:05:47 -0300

    Adicionando menu

--- Secao_15_Manipulacao_de_dados/4 - Lista de tarefas/app/src/main/java/dionizio/victor/listadetarefas/activity/AdicionarTarefaActivity.java
+++ Secao_15_Manipulacao_de_dados/4 - Lista de tarefas/app/src/main/java/dionizio/victor/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package dionizio.victor.listadetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import dionizio.victor.listadetarefas.R;
 
@@ -12,5 +16,25 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                Toast.makeText(
+                        AdicionarTarefaActivity.this,
+                        "Item Salver",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
