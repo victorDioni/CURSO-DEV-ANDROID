77ba8294d045ba94136c0402f0c02959e6a34068
Victor Dionizio <victor.dionizio28@gmail.com>
2021-12-22 16:16:16 -0300

    Recuperando dados

--- Secao_16_Firebase/app/src/main/java/dionizio/victor/firebaseapp/MainActivity.java
+++ Secao_16_Firebase/app/src/main/java/dionizio/victor/firebaseapp/MainActivity.java
@@ -1,14 +1,61 @@
 package dionizio.victor.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Permite salvar dados no firebase
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference usuarios = reference.child("usuarios");
+        DatabaseReference produtos = reference.child("produtos");
+
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // se tiver qualquer mudança na tabela usuarios, esse método ira nos informar
+                Log.i("FIREBASE", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // tratar erros ao tentar atualizar
+            }
+        });
+
+        // Salvando dados no Firebase
+//        reference.child("pontos").setValue("100");
+//
+//        DatabaseReference usuarios = reference.child("usuarios");
+//        DatabaseReference produtos = reference.child("produtos");
+//
+//        Usuario usuario = new Usuario();
+//        usuario.setNome("Victor");
+//        usuario.setSobrenome("Dionizio");
+//        usuario.setIdade(23);
+//
+//        usuarios.child("001").setValue(usuario);
+//
+//        Produtos produto = new Produtos();
+//        produto.setDescricao("Redmi note 10");
+//        produto.setMarca("Xiaomi");
+//        produto.setPreco(1800.00);
+//
+//        produtos.child("001").setValue(produto);
     }
 }
---
+++ Secao_16_Firebase/app/src/main/java/dionizio/victor/firebaseapp/Produtos.java
@@ -0,0 +1,34 @@
+package dionizio.victor.firebaseapp;
+
+public class Produtos {
+    private String descricao;
+    private String marca;
+    private Double preco;
+
+    public Produtos() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
