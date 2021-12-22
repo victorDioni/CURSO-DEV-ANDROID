58edf07ab9c4369a97fce658fbca65be87a26552
Victor Dionizio <victor.dionizio28@gmail.com>
2021-12-22 16:38:37 -0300

    Cadastro e verificacao de usuario logado

--- Secao_16_Firebase/app/src/main/java/dionizio/victor/firebaseapp/MainActivity.java
+++ Secao_16_Firebase/app/src/main/java/dionizio/victor/firebaseapp/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,26 +21,52 @@ public class MainActivity extends AppCompatActivity {
     // Permite salvar dados no firebase
     private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
+    //Permite gerenciar usuarios
+    private FirebaseAuth usuario = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference usuarios = reference.child("usuarios");
-        DatabaseReference produtos = reference.child("produtos");
+        // Verifica usuario logado
+        if(usuario.getCurrentUser() != null){
+            Log.i("CREATE USER", "Usuário logado!");
+        }else{
+            Log.i("CREATE USER", "Usuário não logado!");
+        }
 
-        usuarios.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // se tiver qualquer mudança na tabela usuarios, esse método ira nos informar
-                Log.i("FIREBASE", snapshot.getValue().toString());
-            }
+        // Cadastro de usuario
+//        usuario.createUserWithEmailAndPassword(
+//                "victor@gmail.com", "vic12345")
+//                .addOnCompleteListener(MainActivity.this,
+//                        new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                if(task.isSuccessful()){
+//                                    Log.i("CREATE USER", "Sucesso ao cadastrar usuário");
+//                                }else{
+//                                    Log.i("CREATE USER", "Erro ao cadastrar usuário");
+//                                }
+//                            }
+//                        });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // tratar erros ao tentar atualizar
-            }
-        });
+        // Recuperar dados
+//        DatabaseReference usuarios = reference.child("usuarios");
+//        DatabaseReference produtos = reference.child("produtos");
+//
+//        usuarios.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                // se tiver qualquer mudança na tabela usuarios, esse método ira nos informar
+//                Log.i("FIREBASE", snapshot.getValue().toString());
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                // tratar erros ao tentar atualizar
+//            }
+//        });
 
         // Salvando dados no Firebase
 //        reference.child("pontos").setValue("100");
