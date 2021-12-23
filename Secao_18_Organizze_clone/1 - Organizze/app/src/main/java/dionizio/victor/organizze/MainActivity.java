dfabf65b906686d4c4e12967076805fb7fdd9b59
Victor Dionizio <victor.dionizio28@gmail.com>
2021-12-23 12:05:32 -0300

    Criando interface de cadastro de login

--- Secao_18_Organizze_clone/1 - Organizze/app/src/main/java/dionizio/victor/organizze/MainActivity.java
+++ Secao_18_Organizze_clone/1 - Organizze/app/src/main/java/dionizio/victor/organizze/MainActivity.java
@@ -2,10 +2,15 @@ package dionizio.victor.organizze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+import dionizio.victor.organizze.activity.CadastroActivity;
+import dionizio.victor.organizze.activity.LoginActivity;
 
 public class MainActivity extends IntroActivity {
 
@@ -41,5 +46,20 @@ public class MainActivity extends IntroActivity {
                 .build()
         );
 
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build()
+        );
+
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastro(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
     }
 }
