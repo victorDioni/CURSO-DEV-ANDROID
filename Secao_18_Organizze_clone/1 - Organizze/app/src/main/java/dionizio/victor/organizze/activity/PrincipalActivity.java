403e083fe77d5bd313fb6a1582ae26ab07af28c8
Victor Dionizio <victor.dionizio28@gmail.com>
2022-03-29 16:03:24 -0300

    Validar e autenticar usuario - parte 1

---
+++ Secao_18_Organizze_clone/1 - Organizze/app/src/main/java/dionizio/victor/organizze/activity/PrincipalActivity.java
@@ -0,0 +1,48 @@
+package dionizio.victor.organizze.activity;
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import dionizio.victor.organizze.R;
+import dionizio.victor.organizze.databinding.ActivityPrincipalBinding;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityPrincipalBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+     binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
+     setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
